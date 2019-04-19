b05f19e46625f7597219875eedbc0838da3b3e20
Lucas Marques <lucas.msilva09@gmail.com>
2019-04-19 17:32:36 -0300

    Creating a method to logout the users after the token expires

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -13,6 +13,7 @@ export class AuthService {
   private readonly API = `${environment.API}/api/user`;
   private authStatusListener = new Subject<boolean>();
   private isAuthenticated = false;
+  private tokenTimer: any;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -31,11 +32,16 @@ export class AuthService {
   login(email: string, password: string) {
     const userData: AuthData = { email, password };
     this.http
-      .post<{ token: string }>(`${this.API}/login`, userData)
+      .post<{ token: string, expiresIn: number }>(`${this.API}/login`, userData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresInDuration * 1000);
+          console.log(expiresInDuration);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
@@ -48,9 +54,11 @@ export class AuthService {
   }
 
   logout(){
+
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 }
